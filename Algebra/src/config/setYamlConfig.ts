ef3a479ecf920ba455e5616dd9cebc2be14d8ccc
0xCaliburSigma <0xcalibursigma@protonmail.com>
2024-03-18 14:40:18 -0400

    appease assembly script compiler, use classes instead of objects

--- Algebra/src/config/setYamlConfig.ts
+++ Algebra/src/config/setYamlConfig.ts
@@ -3,16 +3,16 @@ import path from "path"
 import yaml from "js-yaml"
 
 import {
-  TARGET_CHAIN,
-  supportedChains
+  TARGET_CHAIN
 } from "./chains"
 
 const {
+  configName,
   factory,
   nftPositionManager,
   startBlock,
   apiVersion
-} = supportedChains[TARGET_CHAIN]
+} = TARGET_CHAIN
 
 const filePath = path.join(path.resolve(), 'subgraph.yaml')
 let subgraphConfig = fs.readFileSync(filePath, 'utf8');
@@ -21,17 +21,17 @@ let subgraph = yaml.load(subgraphConfig) as any;
 const subgraphFactory = subgraph.dataSources[0]
 const subgraphNFTPositionManager = subgraph.dataSources[1]
 
-subgraphFactory.network = TARGET_CHAIN
+subgraphFactory.network = configName
 subgraphFactory.source.address = factory
 subgraphFactory.source.startBlock = startBlock
 subgraphFactory.mapping.apiVersion = apiVersion
 
-subgraphNFTPositionManager.network = TARGET_CHAIN
+subgraphNFTPositionManager.network = configName
 subgraphNFTPositionManager.source.address = nftPositionManager
 subgraphNFTPositionManager.source.startBlock = startBlock
 subgraphNFTPositionManager.mapping.apiVersion = apiVersion
 
-subgraph.templates[0].network = TARGET_CHAIN
+subgraph.templates[0].network = configName
 subgraph.templates[0].mapping.apiVersion = apiVersion
 
 const updatedSubgraph = yaml.dump(subgraph);
--- Algebra/src/utils/constants.ts
+++ Algebra/src/utils/constants.ts
@@ -1,14 +1,14 @@
 /* eslint-disable prefer-const */
 import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
-import { supportedChains, TARGET_CHAIN } from "../config/chains"
+import { TARGET_CHAIN } from "../config/chains"
 
 const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-const FACTORY_ADDRESS = supportedChains[TARGET_CHAIN].factory
-const WRAPPED_NATIVE = supportedChains[TARGET_CHAIN].wrappedNative
-const USDC_WMatic_03_POOL = supportedChains[TARGET_CHAIN].wrappedNativeUSDCPool
-const WHITELIST_TOKENS: string[] = supportedChains[TARGET_CHAIN].whitelistTokens
-const STABLE_COINS: string[] = supportedChains[TARGET_CHAIN].stableCoins
+const FACTORY_ADDRESS = TARGET_CHAIN.factory
+const WRAPPED_NATIVE = TARGET_CHAIN.wrappedNative
+const USDC_WMatic_03_POOL = TARGET_CHAIN.wrappedNativeUSDCPool
+const WHITELIST_TOKENS: string[] = TARGET_CHAIN.whitelistTokens
+const STABLE_COINS: string[] = TARGET_CHAIN.stableCoins
 
 let ZERO_BI = BigInt.fromI32(0)
 let ONE_BI = BigInt.fromI32(1)
