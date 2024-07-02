b3be6328da2e4cf410649160b979b99a6e5f368f
0xpercival <percival@camelot.fi>
2024-07-03 00:09:11 +0200

    set default pool.tick to max

--- Algebra/src/mappings/factory.ts
+++ Algebra/src/mappings/factory.ts
@@ -126,6 +126,7 @@ export function handlePoolCreated(event: PoolEvent): void {
   pool.createdAtTimestamp = event.block.timestamp
   pool.createdAtBlockNumber = event.block.number
   pool.liquidityProviderCount = ZERO_BI
+  pool.tick = BigInt.fromI32(887617) // maxTick+1, for null cases
   pool.tickSpacing = BigInt.fromI32(60)
   pool.txCount = ZERO_BI
   pool.liquidity = ZERO_BI
